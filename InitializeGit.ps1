3b63aaf2390c929ce2042871935c6fd4bcc9b0bb
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-15 17:52:33 -0400

    Slowly adding the content for the ignore file.

--- InitializeGit.ps1
+++ InitializeGit.ps1
@@ -4,7 +4,18 @@ function New-Init {
     Set-Location $Path
 
     New-Item -Path $Path -Name ".gitignore" -ItemType "file"
-    Add-Content "C:\Users\dakil\Desktop\Repos\CSharp\ConsoleApps\EFTesting\.gitignore" ""
+    Add-Content "C:\Users\dakil\Desktop\Repos\CSharp\ConsoleApps\EFTesting\.gitignore" "
+    # User-specific files `n
+    *.suo `n
+    *.user `n
+    *.sln.docstates `n
+    # Build results 'n
+    [Dd]ebug/ 'n
+    [Rr]elease/ 'n
+    x64/ 'n
+    [Bb]in/ 'n
+    [Oo]bj/
+    "
     git.exe init 
     git.exe stage -A
     git.exe commit -am  "Initial Commit"

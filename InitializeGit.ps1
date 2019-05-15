b6c4eb9c487773c1cfc432f802a2561834257853
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-05-15 15:34:23 -0400

    Changed it to be more generic. Also added the standard first steps required to set up a remote git repo.

--- InitializeGit.ps1
+++ InitializeGit.ps1
@@ -1,13 +1,16 @@
 function New-Init {
-    param ([String]$FolderName, [String]$NewFolderName)
-    $Location = "C:\Users\dakil\Desktop\Repos\"
+    param ([String]$Path [String]$Origin)
 
-    Set-Location $Location$FolderName
+    Set-Location $Path
 
-    New-Item -Path $Location$FolderName -Name ".gitignore" -ItemType "file"
+    New-Item -Path $Path -Name ".gitignore" -ItemType "file"
     git.exe init 
     git.exe stage -A
     git.exe commit -am  "Initial Commit"
+    git.exe add origin $Origin
+    git.exe remote --v
     git.exe status
-    explorer.exe $Location$FolderName
+    git.exe remote add origin $Origin
+    git.exe push -u origin master
+    explorer.exe $Path
 }
